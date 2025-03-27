12a632e60a2e50bff90a025bd5a0b0e2c226bb79
Emagmunioz <emagmunioz@gmail.com>
2025-03-27 12:13:57 +0100

    RED: Failing test for annual rate

--- src/main/java/dev/cuenta/Account.java
+++ src/main/java/dev/cuenta/Account.java
@@ -11,4 +11,9 @@ public abstract class Account {
         return balance;
     }
 
+@Test
+public void whenCreateAccount_thenAnnualRateIsSet() {
+    Account account = new Account(1000f, 0.05f) {};
+    assertThat(account.getAnnualRate(), is(0.05f));
+}
 }
