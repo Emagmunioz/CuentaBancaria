5fd20ddd2a4558f595d0a4a4d66dd0e3c1305060
Emagmunioz <emagmunioz@gmail.com>
2025-03-27 14:08:46 +0100

    RED: Failing test for deposit reducing overdraft

--- src/test/java/dev/cuenta/CurrentAccountTest.java
+++ src/test/java/dev/cuenta/CurrentAccountTest.java
@@ -8,13 +8,22 @@ public class CurrentAccountTest {
         CurrentAccount account = new CurrentAccount(1000f, 0.05f);
         assertThat(account.getOverdraft(), is(0f));
     }
-    
+
 @Test
 public void whenWithdrawBeyondBalance_thenOverdraftIncreases() {
     CurrentAccount account = new CurrentAccount(1000f, 0.05f);
     account.withdraw(1500f);
     assertThat(account.getBalance(), is(0f));
     assertThat(account.getOverdraft(), is(500f));
+}
+
+@Test
+public void whenDepositWithOverdraft_thenReducesOverdraft() {
+    CurrentAccount account = new CurrentAccount(1000f, 0.05f);
+    account.withdraw(1500f); // sobregiro de 500
+    account.deposit(300f);
+    assertThat(account.getOverdraft(), is(200f));
+    assertThat(account.getBalance(), is(0f));
 }
 
 }
