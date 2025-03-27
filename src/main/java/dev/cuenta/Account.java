beac9af9b9a6905b1e34074a0791f5a9866ea6d1
Emagmunioz <emagmunioz@gmail.com>
2025-03-27 12:12:13 +0100

    GREEN: Implement Account constructor with balance

---
+++ src/main/java/dev/cuenta/Account.java
@@ -0,0 +1,14 @@
+package dev.cuenta;
+
+public abstract class Account {
+    protected float balance;
+    
+    public Account(float balance, float annualRate) {
+        this.balance = balance;
+    }
+    
+    public float getBalance() {
+        return balance;
+    }
+
+}
