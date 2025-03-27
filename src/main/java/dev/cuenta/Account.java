678027ee4095c579d46896a3f52a3c0268817ba9
Emagmunioz <emagmunioz@gmail.com>
2025-03-27 12:18:29 +0100

    GREEN: Implement annual rate in Account

--- src/main/java/dev/cuenta/Account.java
+++ src/main/java/dev/cuenta/Account.java
@@ -2,14 +2,18 @@ package dev.cuenta;
 
 public abstract class Account {
     protected float balance;
+    protected float annualRate;
     
     public Account(float balance, float annualRate) {
         this.balance = balance;
+        this.annualRate = annualRate;
     }
     
     public float getBalance() {
         return balance;
     }
-
-
+    
+    public float getAnnualRate() {
+        return annualRate;
+    }
 }
