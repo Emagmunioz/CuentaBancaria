7c110d8278ae365a1ec7c7c5f64302c5dd9704a2
Emagmunioz <emagmunioz@gmail.com>
2025-03-27 12:16:42 +0100

    delete incorret insertion in Account class

--- src/test/java/dev/cuenta/AccountTest.java
+++ src/test/java/dev/cuenta/AccountTest.java
@@ -2,11 +2,16 @@ package dev.cuenta;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
-public class AccoutTest {
+public class AccountTest {
 
  @Test
     public void whenCreateAccount_thenInitialBalanceIsSet() {
         Account account = new Account(1000f, 0.05f) {};
         assertThat(account.getBalance(), is(1000f));
     }
+    @Test
+public void whenCreateAccount_thenAnnualRateIsSet() {
+    Account account = new Account(1000f, 0.05f) {};
+    assertThat(account.getAnnualRate(), is(0.05f));
+}
 }
