071debb6b0e13fd51fc54e182f4a7899acf317ac
Emagmunioz <emagmunioz@gmail.com>
2025-03-27 12:22:00 +0100

    GREEN: Implement deposit method

--- src/main/java/dev/cuenta/Account.java
+++ src/main/java/dev/cuenta/Account.java
@@ -3,7 +3,17 @@ package dev.cuenta;
 public abstract class Account {
     protected float balance;
     protected float annualRate;
+    protected int numberOfDeposits;
+
+    public void deposit(float amount) {
+        balance += amount;
+        numberOfDeposits++;
+    }
     
+    public int getNumberOfDeposits() {
+        return numberOfDeposits;
+    }
+
     public Account(float balance, float annualRate) {
         this.balance = balance;
         this.annualRate = annualRate;
